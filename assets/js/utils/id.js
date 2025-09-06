67e28e21dc0db4af4bd7d1c3aee6fec7f3ae4316
Misrar <engr.qr@gmail.com>
2025-09-06 03:02:41 +0300

    Delete assets directory

--- assets/js/utils/id.js
+++
@@ -1,8 +0,0 @@
-export function nanoid(size = 10) {
-  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let id = '';
-  crypto.getRandomValues(new Uint8Array(size)).forEach(c => {
-    id += chars[c % chars.length];
-  });
-  return id;
-}
